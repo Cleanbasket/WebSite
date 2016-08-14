9f9692fa13a08363e774d1e12f1fdbdb30f854bb
zeakd <stuzeakd@kaist.ac.kr>
2016-08-14 17:46:25 +0900

    minor fix and alert

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
-  
-  console.log("main!");
   init();
 })
 
 function init() {
   fitHero();
+  $('.alert-notyetopen').click(function (e) {
+    e.preventDefault();
+    alert('서비스 준비중입니다. 빠른 시일내로 찾아뵙겠습니다.');
+  }) 
+
   var swiper = new Swiper('.swiper-container', {
     pagination: '.swiper-pagination',
     nextButton: '.swiper-button-next',
@@ -15,7 +18,6 @@ function init() {
     spaceBetween: 30,
     loop: true
   });
-
 
   $(window).on("resize", function () {
     console.log("resize");
@@ -29,3 +31,7 @@ function fitHero () {
   console.log("win height", windowHeight);
   var $hero = $("#hero").height(windowHeight - headerHeight);
 }
+
+// "alert()"
+//  onClick=
+//   onClick="alert('서비스 준비중입니다. 빠른 시일내로 찾아뵙겠습니다.')"
